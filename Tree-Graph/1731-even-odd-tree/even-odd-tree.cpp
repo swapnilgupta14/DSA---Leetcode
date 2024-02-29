073710552e85cfea5e7db3fbd9b23025e763addc
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-02-29 14:19:07 +0530

    Time: 187 ms (88.98%) | Memory: 149.2 MB (76.25%) - LeetSync

--- Tree-Graph/1731-even-odd-tree/even-odd-tree.cpp
+++ Tree-Graph/1731-even-odd-tree/even-odd-tree.cpp
@@ -1,33 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
-        if (root == nullptr) {
+        if(root == nullptr){
             return true;
         }
-        int level = 0;
-        bool isEvenLevel = true; 
         queue<TreeNode*> q;
         q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            int prevVal = isEvenLevel ? INT_MIN : INT_MAX;
-            for (int i = 0; i < size; ++i) {
+
+        int level = 0;
+        bool evenLevel = true;
+
+        while(!q.empty()){
+            int s = q.size();
+            int previous = (evenLevel) ? INT_MIN : INT_MAX;
+            for(int i =0 ; i<s ; i++){
                 TreeNode* node = q.front();
                 q.pop();
-                if (isEvenLevel && (node->val % 2 == 0 || node->val <= prevVal)) {
+
+                if(evenLevel && ((node->val % 2) == 0 || node->val <= previous) ){
                     return false;
-                } else if (!isEvenLevel && (node->val % 2 == 1 || node->val >= prevVal)) {
+                }else if(!evenLevel && ((node->val %2) == 1 || node->val >= previous)){
                     return false;
                 }
-                prevVal = node->val;
-                if (node->left) {
-                    q.push(node->left);
-                }
-                if (node->right) {
+                previous = node->val;
+                if(node->left){
+                    q.push(node->left);}
+                if(node->right){
                     q.push(node->right);
                 }
             }
-            isEvenLevel = !isEvenLevel;
+            evenLevel = !evenLevel;
+
         }
         return true;
     }

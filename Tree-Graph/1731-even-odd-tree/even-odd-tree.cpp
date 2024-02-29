a16f22b7eddc2e2e4dfa75c39e38bf120370357b
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-02-29 12:33:32 +0530

    Time: 186 ms (89.99%) | Memory: 149.3 MB (76.25%) - LeetSync

--- Tree-Graph/1731-even-odd-tree/even-odd-tree.cpp
+++ Tree-Graph/1731-even-odd-tree/even-odd-tree.cpp
@@ -2,31 +2,23 @@ class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
         if (root == nullptr) {
-            return true; // Empty tree is considered an even-odd tree
+            return true;
         }
-
         int level = 0;
-        bool isEvenLevel = true; // Track expected parity for current level
+        bool isEvenLevel = true; 
         queue<TreeNode*> q;
         q.push(root);
-
         while (!q.empty()) {
             int size = q.size();
-            int prevVal = isEvenLevel ? INT_MIN : INT_MAX; // Compare values based on parity
-
-            // Process nodes at the current level
+            int prevVal = isEvenLevel ? INT_MIN : INT_MAX;
             for (int i = 0; i < size; ++i) {
                 TreeNode* node = q.front();
                 q.pop();
-
-                // Check if the node's value violates the even-odd property
                 if (isEvenLevel && (node->val % 2 == 0 || node->val <= prevVal)) {
                     return false;
                 } else if (!isEvenLevel && (node->val % 2 == 1 || node->val >= prevVal)) {
                     return false;
                 }
-
-                // Update comparison value and add children to queue
                 prevVal = node->val;
                 if (node->left) {
                     q.push(node->left);
@@ -35,11 +27,8 @@ public:
                     q.push(node->right);
                 }
             }
-
-            // Switch parity for the next level
             isEvenLevel = !isEvenLevel;
         }
-
-        return true; // All levels processed without violations
+        return true;
     }
 };

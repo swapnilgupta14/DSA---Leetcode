100581ee1943a245cbd7e7aa39be24dcf92bc3c7
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-03-04 18:52:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (25.21%) - LeetSync

---
+++ Tree-Graph/392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0;
+        int j = 0;
+        if(s == "") return true;
+        while(i < s.size() && j<t.size()){
+            if(s[i] != t[j]){
+                j++;
+            }else if(s[i] == t[j]){
+                i++;
+                j++;
+            }
+            if(i == s.size()) return true;
+        }
+        return false;
+    }
+};

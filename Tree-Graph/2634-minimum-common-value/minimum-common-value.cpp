2970b1976a401f3c91cc504e272fd0545f54fa45
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-03-09 23:18:42 +0530

    Time: 72 ms (54.08%) | Memory: 53 MB (29.59%) - LeetSync

--- Tree-Graph/2634-minimum-common-value/minimum-common-value.cpp
+++ Tree-Graph/2634-minimum-common-value/minimum-common-value.cpp
@@ -3,7 +3,6 @@ public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int i=0; int j=0;
         while(i < nums1.size() && j < nums2.size()){
-            cout << nums1[i] <<" "<<nums2[j]<<endl;
             if(nums1[i] == nums2[j]) return nums1[i];
             if(nums1[i] < nums2[j]) i++;
             if(nums1[i] > nums2[j]) j++;

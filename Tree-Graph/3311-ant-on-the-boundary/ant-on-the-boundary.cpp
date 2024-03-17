5e228fbc2144dffd010cd8ff2d83ea01aafc8c58
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-03-17 15:41:01 +0530

    Time: 4 ms (66.99%) | Memory: 22.1 MB (35.90%) - LeetSync

--- Tree-Graph/3311-ant-on-the-boundary/ant-on-the-boundary.cpp
+++ Tree-Graph/3311-ant-on-the-boundary/ant-on-the-boundary.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int returnToBoundaryCount(vector<int>& nums) {
+        int count =0;
+        for(int i=1; i<nums.size(); i++){
+            nums[i] += nums[i-1];
+            if(nums[i] == 0) count++;
+        }return count;
+    }
+private:
+    int returnToBoundaryCount1(vector<int>& nums) {
         int curr = 0;
         int count = 0;
         for(auto& it: nums){

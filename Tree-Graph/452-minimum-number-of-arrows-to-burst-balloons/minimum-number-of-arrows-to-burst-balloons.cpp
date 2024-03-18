41a79d1cc0739311b8a0749096e0f5bfad79a41b
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-03-18 16:29:20 +0530

    Time: 237 ms (92.79%) | Memory: 93.3 MB (65.76%) - LeetSync

--- Tree-Graph/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ Tree-Graph/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,24 +1,20 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    static bool compareIntervals(const vector<int>& a, const vector<int>& b) {
-        return a[0] < b[0];
+    static bool custom_comp(const vector<int> &a, const vector<int> &b){
+        return a[0]<b[0];
     }
-
     int findMinArrowShots(vector<vector<int>>& points) {
-        if (points.empty()) return 0;
-        sort(points.begin(), points.end(), compareIntervals);
-        int count = 1; 
+        int n = points.size();
+        // [[10,16],[2,8],[1,6],[7,12]]
+        int count = 1;
+        sort(points.begin(), points.end(), custom_comp);
         int end = points[0][1];
-        for (int i = 1; i < points.size(); i++) {
-            if (points[i][0] > end) { 
-                count++;
+        //[[1,6] [2,8] [7,12] [10,16]]
+        for(int i=1; i<n; i++){
+            if(points[i][0] > end){
+                count+=1;
                 end = points[i][1];
-            } else { 
+            }else{
                 end = min(end, points[i][1]);
             }
         }

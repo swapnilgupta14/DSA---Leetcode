eb7850ec935c521b142d319ae8bab10a7e5e6ae8
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-03-18 12:38:19 +0530

    Time: 264 ms (44.10%) | Memory: 93.4 MB (41.78%) - LeetSync

--- Tree-Graph/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ Tree-Graph/452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -11,12 +11,9 @@ public:
 
     int findMinArrowShots(vector<vector<int>>& points) {
         if (points.empty()) return 0;
-
         sort(points.begin(), points.end(), compareIntervals);
-
         int count = 1; 
         int end = points[0][1];
-
         for (int i = 1; i < points.size(); i++) {
             if (points[i][0] > end) { 
                 count++;
@@ -25,7 +22,6 @@ public:
                 end = min(end, points[i][1]);
             }
         }
-
         return count;
     }
 };

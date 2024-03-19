47499fbd67c886a058fc0427b99bad833dfce72c
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-03-19 15:05:47 +0530

    Time: 52 ms (78.11%) | Memory: 37.9 MB (80.77%) - LeetSync

--- Tree-Graph/621-task-scheduler/task-scheduler.cpp
+++ Tree-Graph/621-task-scheduler/task-scheduler.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        vector<int> task_counts(26, 0);
+        vector<int> freq(26, 0);
         for (char task : tasks) {
-            task_counts[task - 'A']++; 
+            freq[task - 'A']++; 
         }
         int s = tasks.size();
-        int max_count = *max_element(task_counts.begin(), task_counts.end());
-        int num_max_tasks = count(task_counts.begin(), task_counts.end(), max_count);
+        int max_count = *max_element(freq.begin(), freq.end());
+        int num_max_tasks = count(freq.begin(), freq.end(), max_count);
         int num_intervals = (max_count - 1) * (n + 1) + num_max_tasks;
         int m = max(num_intervals, s);
         return m;

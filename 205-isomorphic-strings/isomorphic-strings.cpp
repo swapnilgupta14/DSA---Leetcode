c8d4ad74aa66b7d82427ca0f8617b98148ffc669
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-04-02 11:58:39 +0530

    Time: 9 ms (28.10%) | Memory: 9.4 MB (17.52%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         // intution - 
-        // HashMap ds:
-        // r: 3
-        // e: 2
-        // a: 1
-        // p: 0
-        // HashMap dt:
-        // e: 3
-        // l: 2
-        // i: 1
-        // t: 0
+        // HashMap ds:       // HashMap dt:
+        // r: 3              // e: 3     
+        // e: 2              // l: 2
+        // a: 1              // i: 1        
+        // p: 0              // t: 0
         unordered_map<char, int> ds, dt;
         for (auto& c : s) {
             if (ds.count(c) != 0) continue;

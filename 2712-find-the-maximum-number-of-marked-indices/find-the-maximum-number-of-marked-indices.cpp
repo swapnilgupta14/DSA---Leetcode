af5ef6f17f3a756eb81e6ec116df599bcc798ef1
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-04-30 18:47:32 +0530

    Time: 113 ms (66.40%) | Memory: 63.6 MB (76.00%) - LeetSync

--- 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.cpp
+++ 2712-find-the-maximum-number-of-marked-indices/find-the-maximum-number-of-marked-indices.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int maxNumOfMarkedIndices(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        int i=0, j=n/2;
+        while(i<n/2 && j<n){
+            if(2*nums[i]<=nums[j]){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i * 2;
+    }
+private:
+    int maxNumOfMarkedIndicesf(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         int temp=0;
         int n=nums.size();

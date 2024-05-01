91357981f81a65f666534e4db9a17a0b8ef1bb75
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-01 17:41:57 +0530

    Time: 3 ms (42.21%) | Memory: 7.4 MB (65.62%) - LeetSync

---
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        for(int i=0; i<word.size(); i++){
+            if(word[i] == ch){
+                int j=0;
+                while(j < i){
+                    cout << i << " and " << j << "\n";
+                    char temp = word[j];
+                    word[j] = word[i];
+                    word[i] = temp;
+                    j++;
+                    i--;
+                }
+                return word;
+            }
+        }
+        return word;
+    }
+};

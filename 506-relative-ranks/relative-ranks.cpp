864611d2e5010fbc6f177ed74ac4f18d6899f651
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-08 11:04:56 +0530

    Time: 4 ms (89.65%) | Memory: 13.6 MB (89.04%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -1,14 +1,16 @@
+#include <vector>
+#include <string>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
         vector<string> ans(n);
         priority_queue<pair<int, int>> pq;
-        unordered_map<int, string> ranks = {
-            {1, "Gold Medal"},
-            {2, "Silver Medal"},
-            {3, "Bronze Medal"}
-        };
 
         for (int i = 0; i < n; ++i) {
             pq.push({score[i], i});
@@ -18,12 +20,16 @@ public:
         while (!pq.empty()) {
             auto [sc, idx] = pq.top();
             pq.pop();
-            if (rank <= 3) {
-                ans[idx] = ranks[rank];
+            if (rank == 1) {
+                ans[idx] = "Gold Medal";
+            } else if (rank == 2) {
+                ans[idx] = "Silver Medal";
+            } else if (rank == 3) {
+                ans[idx] = "Bronze Medal";
             } else {
                 ans[idx] = to_string(rank);
             }
-            ++rank;
+            rank++;
         }
 
         return ans;

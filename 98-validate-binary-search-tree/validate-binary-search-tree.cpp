ff9ff565a6fa772403d6a09e670176ef04a2b4e2
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-08 19:11:56 +0530

    Time: 8 ms (54.62%) | Memory: 20 MB (99.20%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -9,62 +9,52 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool check(TreeNode* root,long int mini,long int maxi){
-        if(root==NULL) return true;
-        if(root->val<=mini || root->val>=maxi){
-            return false;
-        }
-        bool left=check(root->left,mini,root->val);
-        if(left==false) return false;
-        bool right=check(root->right,root->val,maxi);
-        if(right==false) return false;
+    bool check(TreeNode* node, long int minVal, long int maxVal){
+        if(node == NULL) return true;
+        if(node->val >= maxVal || node->val <= minVal) return false;
+        bool lh = check(node->left, minVal, node->val);
+        if(lh == false) return false;
+        bool rh = check(node->right, node->val, maxVal);
+        if(rh == false) return false;
+        // if(lh==false || rh==false) return false;
         return true;
     }
     bool isValidBST(TreeNode* root) {
-        if(root==NULL) return true;
-        
-        long int maxi=LONG_MAX;
-        long int mini=LONG_MIN;
-        if(check(root,mini,maxi)){
+        if(root == NULL) return true;
+        long int maxVal=LONG_MAX;
+        long int minVal=LONG_MIN;
+        if(check(root, minVal, maxVal)){
             return true;
         }
         return false;
-        
-        
     }
 };
+
 // class Solution {
-// private: 
-//     bool check(TreeNode* node){
-//         if(node == NULL) return true;
-//         bool lh = check(node -> left);
-//         bool rh = check(node-> right);
-//         if(lh && rh){
-//             if(node->left) if(node->val <= node->left->val) return false;
-//             if(node->right) if(node->val >= node->right->val) return false;
-//             return true;
-//         } 
-//         return false;
-//     }
 // public:
+//     bool check(TreeNode* root,long int mini,long int maxi){
+//         if(root==NULL) return true;
+//         if(root->val<=mini || root->val>=maxi){
+//             return false;
+//         }
+//         bool left=check(root->left,mini,root->val);
+//         if(left==false) return false;
+//         bool right=check(root->right,root->val,maxi);
+//         if(right==false) return false;
+//         return true;
+//     }
 //     bool isValidBST(TreeNode* root) {
-//         if(root == NULL) return true;
-//         int value = root->val;
-//         if(check(root) ) return true;
+//         if(root==NULL) return true;
+        
+//         long int maxi=LONG_MAX;
+//         long int mini=LONG_MIN;
+//         if(check(root,mini,maxi)){
+//             return true;
+//         }
 //         return false;
+        
+        
 //     }
 // };

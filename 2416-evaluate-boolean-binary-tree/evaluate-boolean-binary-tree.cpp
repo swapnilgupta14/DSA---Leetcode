86ccada42e8a93b1ce7ecadd716ee86922ed93d6
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-16 11:48:47 +0530

    Time: 11 ms (56.68%) | Memory: 17.6 MB (24.21%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool dfs(TreeNode* node){
+        if(node == NULL) return true;
+        bool lh = dfs(node->left);
+        bool rh = dfs(node->right);
+        if(node->val == 2 || node->val == 3){
+            if(node->val == 3){
+                if(lh == 1 && rh == 1) return true;
+                return false;
+            }else if(node->val == 2){
+                if(lh == 0 && rh == 0) return false;
+                return true;
+            }
+        }else{
+            if(node->val == 1) return true;
+            return false;
+        }
+        return true;
+    }
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(root == NULL) return false;
+        if(root -> left == NULL || root->right == NULL){
+            if(root->val == 1) return true;
+            return false;
+        }
+        bool res = dfs(root);
+        return res;
+    }
+};

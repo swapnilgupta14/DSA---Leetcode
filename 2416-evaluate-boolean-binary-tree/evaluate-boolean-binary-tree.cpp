aac044a49f47151065f3d657ed667e8e51b4157f
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-05-16 14:00:38 +0530

    Time: 8 ms (82.42%) | Memory: 17.2 MB (92.60%) - LeetSync

--- 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -6,34 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 private:
-    bool dfs(TreeNode* node){
-        if(node == NULL) return true;
+    bool dfs(TreeNode* node) {
+        if (node == NULL)
+            return true;
         bool lh = dfs(node->left);
         bool rh = dfs(node->right);
-        if(node->val == 2 || node->val == 3){
-            if(node->val == 3){
-                if(lh == 1 && rh == 1) return true;
-                return false;
-            }else if(node->val == 2){
-                if(lh == 0 && rh == 0) return false;
+        if (node->val == 3) {
+            if (lh == 1 && rh == 1)
                 return true;
-            }
-        }else{
-            if(node->val == 1) return true;
             return false;
         }
-        return true;
+        if (node->val == 2) {
+            if (lh == 0 && rh == 0)
+                return false;
+            return true;
+        }
+        if (node->val == 1)
+            return true;
+        return false;
     }
+
 public:
     bool evaluateTree(TreeNode* root) {
-        if(root == NULL) return false;
-        if(root -> left == NULL || root->right == NULL){
-            if(root->val == 1) return true;
+        if (root == NULL)
+            return false;
+        if (root->left == NULL || root->right == NULL) {
+            if (root->val == 1)
+                return true;
             return false;
         }
         bool res = dfs(root);

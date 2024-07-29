1efe1eb84d7ea92e2296129c4713061abd0c4c62
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-07-29 19:19:52 +0530

    Time: 8 ms (65.68%) | Memory: 19.1 MB (25.65%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,52 @@
 class Solution {
-public:
-    vector<int> majorityElement(vector<int>& nums) {
-        // brute - MLE 
+private:
+    vector<int> majorityElement_(vector<int>& nums) {
+        // brute - using Hashmap
         int n = nums.size();
 
-        vector<int>ans;
+        vector<int> ans;
         unordered_map<int, int> mp;
 
-        for (int it: nums){
-            mp[it] +=1;
+        for (int it : nums) {
+            mp[it] += 1;
         }
-        for(pair it: mp){
-            if(it.second > n/3) ans.push_back(it.first);
+        for (pair it : mp) {
+            if (it.second > n / 3)
+                ans.push_back(it.first);
+        }
+        return ans;
+    }
+
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+
+        if (n < 3) {
+            if (n == 2) {
+                if (nums[0] == nums[1]) {
+                    ans.push_back(nums[0]);
+                    return ans;
+                }
+            }
+            return nums;
+        }
+
+        sort(nums.begin(), nums.end());
+        int i = 0;
+        int j = 1;
+
+        while (i < n && j < n) {
+            if (nums[i] != nums[j]) {
+                i++;
+                j++;
+            } else {
+                while (j < n && nums[j] == nums[i]) j++;
+                if ((j - i ) > n / 3)
+                    ans.push_back(nums[i]);
+                i = j;
+                j ++;
+            }
         }
         return ans;
     }

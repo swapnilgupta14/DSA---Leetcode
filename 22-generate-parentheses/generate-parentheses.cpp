5f006cd33eb5c952adea6c3ed51b3c23e39ce34b
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-08-04 23:32:45 +0530

    Time: 7 ms (22.16%) | Memory: 12 MB (85.84%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,30 @@
+class Solution {
+private:
+    void backtrack(int opening, int closing, vector<string>& ans, string& temp) {
+        cout << opening << ", " << closing << " \n";
+        if (opening > closing) return;
+        
+        if (opening == 0 && closing == 0) {
+            ans.push_back(temp);
+            return;
+        }
+        if (opening) {
+            temp += "(";
+            backtrack(opening - 1, closing, ans, temp);
+            temp.pop_back();
+        }
+        if (closing) {
+            temp += ")";
+            backtrack(opening, closing - 1, ans, temp);
+            temp.pop_back();
+        }
+    }
+
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string temp;
+        backtrack(n, n, ans, temp);
+        return ans;
+    }
+};

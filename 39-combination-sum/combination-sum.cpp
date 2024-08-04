1bc690e2a875df4f4869ab12abfedd514585cbc3
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-08-05 04:22:25 +0530

    Time: 4 ms (71.71%) | Memory: 12.4 MB (68.22%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,31 +1,26 @@
-// Input: candidates = [2,3,6,7], target = 7
-// Output: [[2,2,3],[7]]
-// Explanation:
-// 2 and 3 are cand idates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
-// 7 is a candidate, and 7 = 7.
-// These are the only two combinations.
 class Solution {
-public:
-    void backtrack(vector<int>& candidates, int target, vector<int>& temp, 
-                    vector<vector<int>> &ans, int sum, int ind){
-        if(sum  > target) return;
-        if(sum == target){
+private:
+    void backtrack(int idx, int sum, vector<int>& candidates, int target,
+                   vector<vector<int>>& ans, vector<int>& temp) {
+        int n = candidates.size();
+        if (sum > target || idx >= n)
+            return;
+        if (sum == target) {
             ans.push_back(temp);
             return;
         }
-        for(int i=ind; i<candidates.size(); i++){
-            temp.push_back(candidates[i]);
-            sum += candidates[i];
-            backtrack(candidates, target, temp, ans, sum, i);
+        for(int it = idx; it < n; it ++){
+            temp.push_back(candidates[it]);
+            backtrack(it, sum + candidates[it], candidates, target, ans, temp);
             temp.pop_back();
-            sum -= candidates[i];
         }
-
     }
+
+public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> temp;
-        backtrack(candidates, target, temp, ans, 0, 0);
+        backtrack(0, 0, candidates, target, ans, temp);
         return ans;
     }
 };

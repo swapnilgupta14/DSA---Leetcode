03112168b9c31635dd18b885dbe067045eb96679
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-08-06 00:58:50 +0530

    Time: 12 ms (13.55%) | Memory: 17.8 MB (9.74%) - LeetSync

---
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+        for(auto it : details){
+            string s;
+            s+= it[11];
+            s+= it[12];
+            int n = stoi(s);
+            if(n > 60) count += 1;
+        }
+        return count;
+    }
+};

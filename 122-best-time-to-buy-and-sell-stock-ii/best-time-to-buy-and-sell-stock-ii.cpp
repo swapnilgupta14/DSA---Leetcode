ca6001735c0901ff4abbed4d7173babc19c47bfb
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-08-07 13:48:10 +0530

    Time: 18 ms (5.51%) | Memory: 20.9 MB (5.29%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,17 +1,26 @@
+//recursion
 class Solution {
+private:
+    int fun(int ind,int buy,int n,vector<int>& prices, vector<vector<long>> &dp){
+        if(ind >= n) return 0;
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        long profit = 0;
+        if(buy){
+            int take = -1 * prices[ind]+ fun(ind+1,0,n,prices, dp);
+            int notTake = fun(ind+1,1,n,prices, dp);
+            profit = max(take, notTake);
+        }
+        else{
+            int take= prices[ind]+ fun(ind+1,1,n,prices, dp);
+            int notTake= fun(ind+1,0,n,prices, dp);
+            profit = max(take,notTake);
+        }
+        return dp[ind][buy] = profit;
+    }
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        
-        int min = prices[0];
-        int m = 0;
-
-        for(int i=0; i<n-1; i++){
-            if(prices[i] < prices[i+1]){
-                m += (prices[i+1] - prices[i]);
-            }
-        }
-
-        return m;
+        int n= prices.size();
+        vector<vector<long>> dp(n, vector<long>(2, -1));
+        return fun(0,1,n,prices, dp);
     }
 };

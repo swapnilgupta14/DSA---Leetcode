d09c6c55881d63c1a57857c4f4ae037b25bfeb06
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-11-06 09:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (67.88%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,15 +1,10 @@
 class Solution {
-private:
-    int helper(char a, char b){
-        if(a != b) return 1;
-        return 0;
-    }
 public:
     int minChanges(string s) {
         int quan = 0;
         long int size = s.size();
         for(long int i=0; i<size; i+=2){
-            quan += helper(s[i], s[i+1]);
+            quan += (s[i] != s[i+1] ? 1 : 0);
         }
         return quan;
     }
